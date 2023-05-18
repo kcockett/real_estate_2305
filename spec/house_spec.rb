505042df7fefabbf508f8403494431eed40573dd
Kaina Cockett <kainacockett@gmail.com>
2023-05-18 09:49:15 -0600

    test:  checking house price as int

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -2,8 +2,17 @@ require "./lib/room"
 require "./lib/house"
 
 RSpec.describe House do
-    it "exists" do
-        house = House.new("$400000", "123 sugar lane")
-        expect(house).to be_a House
+        describe "Iteration 2" do
+        it "exists" do
+            house = House.new("$400000", "123 sugar lane")
+
+            expect(house).to be_a House
+        end
+
+        it "house.price returns and integer of value" do
+            house = House.new("$400000", "123 sugar lane")
+                
+            expect(house.price).to eq(400000)
+        end
     end
 end
