559f34dbf5dab9fe5949fe73e18d5d9f838058a1
Kaina Cockett <kainacockett@gmail.com>
2023-05-18 09:55:21 -0600

    feature: add price method to House

--- lib/house.rb
+++ lib/house.rb
@@ -1,7 +1,14 @@
 class House
+
+    attr_accessor :address, :price
     
     def initialize(price, address)
         @price = price
         @address = address
+    end
+
+    def price
+        value = @price.delete "$"
+        value.to_i
     end
 end
