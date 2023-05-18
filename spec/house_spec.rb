9a1dd32d40e43e7a9df11ead0a610599ae128063
Kaina Cockett <kainacockett@gmail.com>
2023-05-18 10:31:47 -0600

    test: add rooms_by_category

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -50,5 +50,19 @@ RSpec.describe House do
             expect(house_2.above_market_average?).to eq(true)
         end
 
+        it "show rooms by category" do
+            house = House.new("$400000", "123 sugar lane")
+            room_1 = Room.new(:bedroom, 10, '13')
+            room_2 = Room.new(:bedroom, 11, '15')  
+            room_3 = Room.new(:living_room, 25, '15')
+            room_4 = Room.new(:basement, 30, '41')
+            house.add_room(room_1)
+            house.add_room(room_2) 
+            house.add_room(room_3) 
+            house.add_room(room_4) 
+
+            expect(house.rooms_from_category(:bedroom)).not_to be_empty
+        end
+
     end
 end
